29c4337a9abeb8edd5bedc38b311a7757629e9ed
Артур Кустяев <95183545+ArturKustyaev@users.noreply.github.com>
2023-11-01 23:48:12 +0400

    update workflow

--- nodespecific.ts
+++
@@ -1,5 +0,0 @@
-import { Buffer } from 'buffer'
-import Process from 'process'
-
-globalThis.Buffer = Buffer
-globalThis.process = Process
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import inject from '@rollup/plugin-inject'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
-import svgr from 'vite-plugin-svgr'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  publicDir: 'public',
-  server: { port: 3002 },
+  plugins: [react()],
   base: '/snake-game/',
-  build: {
-    outDir: 'build',
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'], process: 'process' })],
-    },
-  },
-  plugins: [react(), eslint(), svgr(), tsconfigPaths(), checker({ typescript: true })],
 })
